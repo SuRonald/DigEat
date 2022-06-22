26da517dc93a987d45e05c3955ef0d0101129986
samuel-sshi <samuelchris12350@gmail.com>
2022-06-22 23:16:47 +0700

    added recyclerview for order page

---
+++ app/src/main/java/com/example/digeat/customer/CustomerOrderAdapter.java
@@ -0,0 +1,31 @@
+package com.example.digeat.customer;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CustomerOrderAdapter extends RecyclerView.Adapter{
+
+    Context context;
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/example/digeat/customer/CustomerOrderViewHolder.java
@@ -0,0 +1,13 @@
+package com.example.digeat.customer;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CustomerOrderViewHolder extends RecyclerView.ViewHolder{
+
+    public CustomerOrderViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
