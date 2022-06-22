fbb3d27484ed0745571e354395fde423449365f6
samuel-sshi <samuelchris12350@gmail.com>
2022-06-22 22:28:59 +0700

    modified logout button in partner profile page and consumer profile page

--- app/src/main/java/com/example/digeat/partner/PartnerProfilePage.java
+++ app/src/main/java/com/example/digeat/partner/PartnerProfilePage.java
@@ -5,20 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.digeat.LoginPage;
 import com.example.digeat.R;
+import com.example.digeat.customer.CustomerQRScan;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import static com.example.digeat.databases.DataVault.currentUser;
 
-public class PartnerProfilePage extends AppCompatActivity {
+public class PartnerProfilePage extends AppCompatActivity implements View.OnClickListener{
 
     BottomNavigationView bottomNavigationView;
     Intent movePage;
+    Button btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_partner_profile_page);
 
+        btnLogout = findViewById(R.id.buttonLogout);
+        btnLogout.setOnClickListener(this);
         bottomNavigationView = findViewById(R.id.bottomNavbar);
         Menu menu = bottomNavigationView.getMenu();
         menu.getItem(3).setChecked(true);
@@ -46,5 +54,14 @@ public class PartnerProfilePage extends AppCompatActivity {
             }
             return true;
         });
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() ==  R.id.buttonLogout){
+            currentUser = null;
+            movePage = new Intent(this, LoginPage.class);
+            startActivity(movePage);
+        }
     }
 }
